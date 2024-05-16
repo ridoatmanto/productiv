cdfa5af6bc3bdd6a6a596763e4f4ca30c2b6552d
Rido Atmanto <ridoatmanto@gmail.com>
2024-05-16 13:21:44 +0700

    Delete src/utils.tsx

--- src/utils.tsx
+++
@@ -1,8 +0,0 @@
-interface Person {
-  name: string;
-  imageId: string;
-}
-
-export function getImageUrl(person: Person, size = "s") {
-  return "https://i.imgur.com/" + person.imageId + size + ".jpg";
-}
