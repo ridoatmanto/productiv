f53d0ee8771c9ba09a219a6b03a2ed20d68dea27
Rido Atmanto <ridoatmanto@gmail.com>
2024-06-03 16:35:20 +0700

    delete src/utils directory

--- src/utils/hello.tsx
+++
@@ -1,3 +0,0 @@
-export function SayHello(name = "User") {
-  return "Hellow, " + name;
-}
--- src/utils/title-case.tsx
+++
@@ -1,9 +0,0 @@
-export function TitleCase(text = "") {
-  return text
-    .toLowerCase()
-    .split(" ")
-    .map(function (word) {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-}
